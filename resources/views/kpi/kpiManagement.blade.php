1449b7392fb2a2e6db64853d86571e7d9d716136
chounry <oun.chounry.kh@gmail.com>
2019-10-19 21:58:23 +0700

    create : kpi managment page

--- resources/views/kpi/kpiManagement.blade.php
+++ resources/views/kpi/kpiManagement.blade.php
@@ -3,24 +3,130 @@
     <script type="text/javascript" src="{{asset('js/kpiManagement.js')}}"></script>
 @endsection
 @section('css')
+<style>
+    #create-kpi-container{
+        margin-bottom: 25px;
+        margin-top: 40px;
+    }
 
+    .middle-row {
+        background: red;
+        height: 10px;
+    }
+
+    .data-container-row{
+        box-shadow: 2px 12px 8px -8px rgba(0,0,0,.25);
+    }
+
+    table > thead{
+        background: #5F71E4;
+        height: 58px;
+        border-radius: 3px;
+
+    }
+
+    .table thead tr th {
+        color: white;
+        vertical-align: middle;
+        font-weight: bold;
+    }
+
+    .table th, .table td{
+        text-align: center;
+    }
+
+    .table th:nth-last-child(1),.table td:nth-last-child(1){
+        text-align: end;
+    }
+
+    .status {
+        color: red;
+    }
+
+    .status-blue{
+        color: #5F71E4;
+    }
+
+</style>
 @endsection
 @section('content')
     @include('layouts.headers.cards')
     <div class="container-fluid" style="margin-top: 10px">
-        @foreach ($questions as $q)
-            <div class="row align-items-center">
-                <div class="col-10">
-                    <div id="text">{{ $q->question}}</div>
-                </div>
-                <div class="col-2">
-                    <input type="checkbox" data-toggle="toggle" id="activeKpi" >
-                </div>
-                <div class="col-12">
-                    <hr>
-                </div>
+        <div id="create-kpi-container" class="row justify-content-end">
+            <div class="col" style="text-align:end;">
+                <button class="btn btn-success" type="button">
+                    Create KPI
+                </button>
             </div>
-        @endforeach
+        </div>
+        <div class="row">
+            <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Status</th>
+                    <th scope="col"># of Question</th>
+                    <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="data-container-row">
+                    <td>Mark</td>
+                    <td>Mark</td>
+                    <td class="status">disable</td>
+                    <td>@mdo</td>
+                    <td> 
+                        <div class="dropdown">
+                            <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-ellipsis-v"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                <a class="dropdown-item" href="#">View</a>
+                                <a class="dropdown-item" href="#">Activate</a>
+                            </div>
+                        </div>
+                    </td>
+                    </tr>
+                    <tr class="middle-row"></tr>
+                    <tr class="data-container-row">
+                    <td>Mark</td>
+                    <td>Jacob</td>
+                    <td class="status-blue">active</td>
+                    <td>@fat</td>
+                    <td> 
+                        <div class="dropdown">
+                            <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-ellipsis-v"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                <a class="dropdown-item" href="#">View</a>
+                                <a class="dropdown-item" href="#">Deactivate</a>
+                            </div>
+                        </div>
+                    </td>
+                    </tr>
+                    <tr class="middle-row"></tr>
+                    <tr class="data-container-row">
+                    <td>Mark</td>
+                    <td>Larry</td>
+                    <td class="status">disable</td>
+                    <td>@twitter</td>
+                    <td>
+                        <div class="dropdown">
+                            <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-ellipsis-v"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                <a class="dropdown-item" href="#">View</a>
+                                <a class="dropdown-item" href="#">Activate</a>
+                            </div>
+                        </div>
+                    </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
    
      <div class="container-fluid" style="margin-top: 10px">
