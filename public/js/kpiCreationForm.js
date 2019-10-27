0aa4c5034f604cd021bcc0a9b3a00d178943fe06
Dara Pon <pondara69@gmail.com>
2019-10-28 00:40:13 +0700

    update : kpi form version 2

--- public/js/kpiCreationForm.js
+++ public/js/kpiCreationForm.js
@@ -7,9 +7,11 @@ $(document).ready(function(){
     var activateButton = $($('.activate-btn').children())
     
     activateButton.click(function(){
+        // when use click on that activate ubtton
         questionStr = $(this).parent().siblings().first().text()
         questionId = $(this).attr('q-id')
         if($(this).attr('aria-pressed') == 'true'){
+            // if the button already activated -> remove its associated question in the selected table
             idStr = `#${questionId}`
             $(idStr).remove()
         }else{
@@ -18,18 +20,67 @@ $(document).ready(function(){
     })
 
     function createSelectedQuestion(question, maxScore, activateBtn, qId){
-        // console.log($($(activateBtn).children()[0]).attr('aria-pressed'));
-        if($(activateBtn).attr('aria-pressed') == 'true'){
-            return 
-        }
-        var selectedQuestionElement = $(`<tr id='${qId}'><td>${question}</td><td>${maxScore}</td><td class='remove-btn'>remove</td></tr>`);
+        var success = false
+        var maxScoreModal = $('#max-score-form-modal')
 
-        tableSelectedQuestion.append(selectedQuestionElement)
-        // console.log(selectedQuestionElement.children('.remove-btn'));
-        $(selectedQuestionElement.children('.remove-btn')[0]).click(function(){
-            $(activateBtn).trigger('click')
-            selectedQuestionElement.remove()
+        console.log($(activateBtn).attr('aria-pressed'))
+        if($(activateBtn).attr('aria-pressed') == 'true'){
+            return;
+        }
+        showBorderInputMaxScoreError(false)
+        $("#max-score-input").val('')
+        // trigger modal
+        maxScoreModal.modal('show')
+        maxScoreModal.on('hidden.bs.modal', function (e) {
+            if(!success){
+                maxScoreModal.unbind()
+                $(activateBtn).trigger('click')
+                $("#save_max_score_btn").unbind()
+            }
         })
+
+        $("#save_max_score_btn").click(function(){
+
+            inputMaxScore = $("#max-score-input").val()
+            if(!isNaN(+inputMaxScore) && +inputMaxScore > 0){
+                console.log("nothong")
+                var selectedQuestionElement = $(`<tr id='${qId}'><td>${question}</td><td>${maxScore}</td><td class='remove-btn'>remove</td></tr>`);
+                success = true
+                tableSelectedQuestion.append(selectedQuestionElement)
+                // console.log(selectedQuestionElement.children('.remove-btn'));
+                $(selectedQuestionElement.children('.remove-btn')[0]).click(function(){
+                    
+                    maxScoreModal.unbind()
+                    $(activateBtn).trigger('click')
+                    selectedQuestionElement.remove()
+                })
+                $("#save_max_score_btn").unbind()
+                maxScoreModal.modal('hide')
+            }else{
+                showBorderInputMaxScoreError(true)
+            }
+        })
+
+            // var selectedQuestionElement = $(`<tr id='${qId}'><td>${question}</td><td>${maxScore}</td><td class='remove-btn'>remove</td></tr>`);
+
+            // tableSelectedQuestion.append(selectedQuestionElement)
+            // // console.log(selectedQuestionElement.children('.remove-btn'));
+            // $(selectedQuestionElement.children('.remove-btn')[0]).click(function(){
+            //     $(activateBtn).trigger('click')
+            //     selectedQuestionElement.remove()
+            // })
+    }
+    $("#max-score-input").keypress(function(){
+        showBorderInputMaxScoreError(false)
+    })
+
+    function showBorderInputMaxScoreError(isDisplay){
+        if(isDisplay){
+            $("#max-score-input").css('border-color', 'red')
+        }
+        else{
+            $("#max-score-input").css('border-color', 'unset')
+        }
     }
 
 })
--- resources/views/kpi/kpiCreationForm.blade.php
+++ resources/views/kpi/kpiCreationForm.blade.php
@@ -235,4 +235,25 @@
             </div>
         </div>
     </div>
+          
+    <!-- Modal -->
+    <div class="modal fade" id="max-score-form-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Enter a max score</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <input type="number" class="form-control" id="max-score-input"/>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" id="save_max_score_btn" class="btn btn-primary">Enter</button>
+        </div>
+        </div>
+    </div>
+    </div>
 @endsection
